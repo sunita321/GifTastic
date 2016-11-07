e26477e3893320aaa57dc2111c688bf0e2bb41d8
SunitaGirl <sramnari@gmail.com>
2016-11-06 20:18:34 -0500

    Adding giph query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,81 +1,107 @@
-// begin document.ready function
-$(document).ready(function() 
+// Wait for page to load
+$(document).ready(function()
 {
-	// Initial array of celebrities
-	var celebrities = ['kanye west', 'jennifer lawrence', 'mindy kaling', 'arnold schwarzenegger'];
+	
+// Initial array of topics
+var topics = ['The Matrix', 'The Notebook', 'Mr. Nobody', 'The Lion King'];
 
-	// Generic function for displaying movie data 
-	function renderButtons()
-	{ 
-		// Deletes the celebrities prior to adding new celebrities (this is necessary otherwise you will have repeat buttons)
-		$('#buttonsView').empty();
-		// Loops through the array of celebrities
-		for (var i = 0; i < celebrities.length; i++){
-			// Then dynamicaly generates buttons for each movie in the array
-			// Note the jQUery syntax here... 
-		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-		    a.addClass('star'); // Added a class 
-		    a.attr('data-name', celebrities[i]); // Added a data-attribute
-		    a.text(celebrities[i]); // Provided the initial button text
-		    $('#celebritiesView').append(a); // Added the button to the HTML
-		}
-	}
-	// ========================================================
-	// This function handles events where one button is clicked
-	$('#addMovie').on('click', function(){
-		// This line of code will grab the input from the textbox
-		var celebrity = $('#giphyInput').val().trim();
-		// The movie from the textbox is then added to our array
-		celebrities.push(celebrity);
-		
-		// Our array then runs which handles the processing of our movie array
-		renderButtons();
-		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
-		return false;
-	})
-	// ========================================================
-	// This calls the renderButtons() function
-	renderButtons();
+//Generic function for displaying gifs
 
-
-//Generic function for displaying movie data 
-// displayCelebrityGif function now re-renders the HTML to display the appropriate content. 
-//function displayCelebrityGif()
-//{
-	var celebrity = $(this).attr('data-name');
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + "kanye+west" + "&api_key=dc6zaTOxFJmzC&limit=1&offset=0";
-	// http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC&limit=1&offset=0
-
+function displaytopicGif()
+{
+	var topic = $(this).attr('data-name');
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10&offset=0";
 
 
 	$.ajax({url: queryURL, method: 'GET'})
 	.done(function(response) 
 	{
 
-		// Creates a generic div to hold the celebrity
-			var celebrityDiv = $('<div class="celebrity">');
-	     console.log(response);
-	     for (i=0; i < response.data.length; i++) 
-	     {
-	     	var stillImage = response.data[i].images.fixed_height_still.url;
-	     	console.log(stillImage);
+		// Creates a generic div to hold the topic
+		var topicDiv = $('<div class="topic">');
+		console.log(response);
+		for (i=0; i < response.data.length; i++) 
+		{
+			var stillImage = response.data[i].images.fixed_height_still.url;
+			console.log(stillImage);
 
-	     	var rating = response.data[i].rating;
+			var rating = response.data[i].rating;
 
-	     	console.log(rating);
-	     	var image = $("<img>").attr("src", stillImage);
+			console.log(rating);
+			var image = $("<img>").attr("src", stillImage);
 
-	     	celebrityDiv.append(image);
+			topicDiv.append(image);
 
-	     	// Puts the entire celebrity above the previous celebrities.
-			$('#gifView').prepend(celebrityDiv);
+			// Puts the entire topic above the previous celebrities.
+			$('#gifView').prepend(topicDiv);
+		}
+	});
+}
 
-
+//displaytopicGif();
 	
-	};
 
-})
+
+
+
+// Generic function for displaying topic data 
+function renderButtons()
+{
+	// Deletes the topics prior to adding new topics (this is necessary otherwise you will have repeat buttons)
+	$('#topicsView').empty();
+
+	// Loops through the array of topics
+	for (var i = 0; i < topics.length; i++)
+	{
+
+		// Then dynamicaly generates buttons for each topic in the array
+
+		// Note the jQUery syntax here... 
+	    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+	    a.addClass('topic'); // Added a class 
+	    a.attr('data-name', topics[i]); // Added a data-attribute
+	    a.text(topics[i]); // Provided the initial button text
+	    $('#topicsView').append(a); // Added the button to the HTML
+	}
+}
+
+	// ========================================================
+
+	// This function handles events where one button is clicked
+	$('#addTopic').on('click', function(){
+
+		console.log('button clicked');
+
+		// This line of code will grab the input from the textbox
+		var topic = $('#topicInput').val().trim();
+
+		console.log(topic);
+
+
+
+		// The topic from the textbox is then added to our array
+		topics.push(topic);
+		
+		// Our array then runs which handles the processing of our topic array
+		renderButtons();
+
+		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		return false;
+	})
+
+	// ========================================================
+
+	// This calls the renderButtons() function
+	renderButtons()
+
+
+
+
+// Generic function for displaying the movieInfo
+	$(document).on('click', '.topic', displaytopicGif);
+
+
+
 
 });
 
-	
