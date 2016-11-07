6587e1260c4b7cd3bfa1b24c09be423b4c47331d
SunitaGirl <sramnari@gmail.com>
2016-11-06 22:46:20 -0500

    added swap gif function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,85 @@
 // Wait for page to load
 $(document).ready(function()
 {
+function swapGif()
+
+
+{
+	//Stop Image 
+	var playImage = $(this).attr('playsrc');
+
+	console.log(playImage);
+
+
+	//Stop Image 
+	var stopImage = $(this).attr('stopsrc');
+
+	console.log(stopImage);
+	
+	
+
+
+
+	if ($(this).attr('playsrc') == $(this).attr('src'))
+	{
+		//This changes the image src
+		$(this).attr('src', stopImage);
+	}
+
+	else
+	{
+		$(this).attr('src', playImage);
+	}
+}
+
+
+//Generic function for displaying gifs
+function displaytopicGif()
+{
+
+	$('#gifView').empty();
+	var topic = $(this).attr('data-name');
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10&offset=0";
+
+
+	$.ajax({url: queryURL, method: 'GET'})
+	.done(function(response) 
+	{
+
+		// Creates a generic div to hold the topic
+		var topicDiv = $('<div class="topicImage">');
+		console.log(response);
+		for (i=0; i < response.data.length; i++) 
+		{
+			var stillImage = response.data[i].images.fixed_height_still.url;
+			console.log(stillImage);
+
+			var playImage = response.data[i].images.fixed_height_downsampled.url;
+			console.log("Moving"+ playImage);
+
+			//var rating = response.data[i].rating;
+
+			//console.log(rating);
+			var image = $("<img>").attr("src", stillImage);
+			image.attr("playsrc", playImage);
+			image.attr("stopsrc", stillImage);
+			
+			topicDiv.append(image);
+
+			// Puts the entire topic above the previous celebrities.
+			$('#gifView').append(topicDiv);
+
+			image.addClass('playClickedGif'); // Added a class to image tag
+
+
+		}	
+	});
+}
+
+
 	
 	// Initial array of topics
 	var topics = ['The Simpsons', 'Bugs Bunny', 'Mickey Mouse', 'Sailor Moon', 'Tom and Jerry'];
-
-	//Generic function for displaying gifs
-
-	function displaytopicGif()
-	{
-
-		$('#gifView').empty();
-		var topic = $(this).attr('data-name');
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10&offset=0";
-
-
-		$.ajax({url: queryURL, method: 'GET'})
-		.done(function(response) 
-		{
-
-			// Creates a generic div to hold the topic
-			var topicDiv = $('<div class="topic">');
-			console.log(response);
-			for (i=0; i < response.data.length; i++) 
-			{
-				var stillImage = response.data[i].images.fixed_height_still.url;
-				console.log(stillImage);
-
-				var playImage = response.data[i].images.fixed_height_downsampled.url;
-				console.log("Moving"+ playImage);
-
-				//var rating = response.data[i].rating;
-
-				//console.log(rating);
-				var image = $("<img>").attr("src", stillImage);
-
-				topicDiv.append(image);
-
-				// Puts the entire topic above the previous celebrities.
-				$('#gifView').append(topicDiv);
-
-				//Click image causes gif to play
-
-				image.addClass('play'); // Added a class to image tag
-
-				// This function handles events where one button is clicked
-				//$('.play').on('click', function()
-				//{
-					//var playImage = response.data[i].images.fixed_height_small_url;
-					//console.log("Moving" + showMoving);
-
-					//var image = $("<img>").attr("src", playImage);
-
-
-				//})
-			}	
-		});
-	}
-
-
-		
-
-
-
 
 	// Generic function for displaying topic data 
 	function renderButtons()
@@ -78,49 +95,44 @@ $(document).ready(function()
 
 			// Note the jQUery syntax here... 
 		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-		    a.addClass('topic'); // Added a class 
+		    a.addClass('topicButton'); // Added a class 
 		    a.attr('data-name', topics[i]); // Added a data-attribute
 		    a.text(topics[i]); // Provided the initial button text
 		    $('#topicsView').append(a); // Added the button to the HTML
 		}
 	}
 
-		// ========================================================
+	// ========================================================
 
-		// This function handles events where one button is clicked
-		$('#addTopic').on('click', function(){
+	// This function handles events where one button is clicked
+	$('#addTopic').on('click', function(){
 
-			console.log('button clicked');
+		console.log('button clicked');
 
-			// This line of code will grab the input from the textbox
-			var topic = $('#topicInput').val().trim();
+		// This line of code will grab the input from the textbox
+		var topic = $('#topicInput').val().trim();
 
-			console.log(topic);
+		console.log(topic);
 
 
+		// The topic from the textbox is then added to our array
+		topics.push(topic);
+		
+		// Our array then runs which handles the processing of our topic array
+		renderButtons();
 
-			// The topic from the textbox is then added to our array
-			topics.push(topic);
-			
-			// Our array then runs which handles the processing of our topic array
-			renderButtons();
+		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		return false;
+	});
 
-			// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
-			return false;
-		})
-
-		// ========================================================
+	// ========================================================
 
 	// This calls the renderButtons() function
-	renderButtons()
-
-
-
+	renderButtons();
 
 	// Generic function for displaying the movieInfo
-	$(document).on('click', '.topic', displaytopicGif);
-
-
+	$(document).on('click', '.topicButton', displaytopicGif);
+	$(document).on('click', '.playClickedGif', swapGif);
 
 
 });
